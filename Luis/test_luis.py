47fc692c21545703e5642a3a59a55666d8655fe4
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-09 09:33:52 +0200

    Update test_luis.py

--- Luis/test_luis.py
+++ Luis/test_luis.py
@@ -18,8 +18,8 @@ import re
 #import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 
-#from Data.performance import test_luis_REST_APIs 
-from performance import test_luis_REST_APIs 
+from Data.performance import test_luis_REST_APIs 
+#from performance import test_luis_REST_APIs 
 
 def test_luis_prediction_bookflight():
     query ="travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
