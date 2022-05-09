abeec1a2ce12cd42e76b4feaa4df57e77cdbe7d3
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-09 10:49:36 +0200

    Create test_Luis.py

---
+++ test_Luis.py
@@ -0,0 +1,19 @@
+import argparse
+import time
+import os
+import json
+import re
+
+
+
+from Data.performance import test_luis_REST_APIs 
+
+def test_luis_prediction_bookflight():
+    query ="travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
+    reply = test_luis_REST_APIs(query)
+    assert reply["prediction"]["topIntent"] == "BookFlight"
+    assert reply["prediction"]["entities"]["Departure"] [0] == "Paris"
+    assert reply["prediction"]["entities"]["Destination"] [0]== "Maldives"
+    assert reply["prediction"]["entities"]["DepartureDate"] [0]== "30/03/2022"
+    assert reply["prediction"]["entities"]["ArrivalDate"] [0]== "30/04/2022"
+    assert reply["prediction"]["entities"]["$instance"]["money"][0]['text'] == "5000£"
