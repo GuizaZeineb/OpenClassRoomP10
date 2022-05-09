a59d90906b003191cf5521d4068d2a0661f96720
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-09 10:36:24 +0200

    Update test_luis.py

--- test_luis.py
+++ test_luis.py
@@ -5,21 +5,8 @@ import json
 import re
 
 
-#from azure.cognitiveservices.language.luis.authoring import LUISAuthoringClient
-#from azure.cognitiveservices.language.luis.authoring.models import ApplicationCreateObject
-#from azure.cognitiveservices.language.luis.runtime import LUISRuntimeClient
-#from msrest.authentication import CognitiveServicesCredentials
-#from functools import reduce
-
-
-
-#import json, time, uuid
-#import requests
-#import http.client, urllib.request, urllib.parse, urllib.error, base64
-
 
 from Data.performance import test_luis_REST_APIs 
-#from performance import test_luis_REST_APIs 
 
 def test_luis_prediction_bookflight():
     query ="travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
