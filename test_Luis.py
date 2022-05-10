38c521f28bbdf45cb035b0f81106977313d7156b
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-11 01:40:15 +0200

    Update test_Luis.py

--- test_Luis.py
+++ test_Luis.py
@@ -12,7 +12,7 @@ from Data.performance import test_luis_REST_APIs
 
 def test_luis_prediction_bookflight():
     query ="travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
-    reply = test_luis_REST_APIs(my_query)
+    reply = test_luis_REST_APIs(query)
     assert reply["prediction"]["topIntent"] == "BookFlight"
     assert reply["prediction"]["entities"]["Departure"] [0] == "Paris"
     assert reply["prediction"]["entities"]["Destination"] [0]== "Maldives"
