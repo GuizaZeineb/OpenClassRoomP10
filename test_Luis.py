e519bcd017252db070a42e71f9e1f44f0c242844
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-11 01:29:35 +0200

    Update test_Luis.py

--- test_Luis.py
+++ test_Luis.py
@@ -9,14 +9,9 @@ import pytest
 from Data.performance import test_luis_REST_APIs 
 
 
-@pytest.fixture
-def my_query():
-    return "travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
 
-#def test_luis_prediction_bookflight():
-def test_luis_prediction_bookflight(my_query):
-
-    #query ="travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
+def test_luis_prediction_bookflight():
+    query ="travel on 30/03/2022 and return on 30/04/2022 from Paris to Maldives with 5000£ "
     reply = test_luis_REST_APIs(my_query)
     assert reply["prediction"]["topIntent"] == "BookFlight"
     assert reply["prediction"]["entities"]["Departure"] [0] == "Paris"
